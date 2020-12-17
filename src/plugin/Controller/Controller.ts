9f156e872c330ded19745e551fa319e200703f5f
nirall <nirall@mail.ru>
2020-12-17 15:52:00 +0500

    Issue #10. Refactoring Controller for several conditions checking

--- src/plugin/Controller/Controller.ts
+++ src/plugin/Controller/Controller.ts
@@ -4,6 +4,19 @@ import Model from '../Model/Model';
 import MakeObservableObject from '../makeObservableObject/MakeObservableObject';
 import ParsingDigits from './blocks/parsingDigits/ParsingDigits';
 
+const isOthersValuesFloat = (item: Controller, parameter: string): boolean => {
+  switch (parameter) {
+    case 'step':
+      return (item.view.parameters.maxValue % 1 !== 0 || item.view.parameters.minValue % 1 !== 0);
+    case 'maxValue':
+      return (item.view.parameters.step % 1 !== 0 || item.view.parameters.minValue % 1 !== 0);
+    case 'minValue':
+      return (item.view.parameters.step % 1 !== 0 || item.view.parameters.maxValue % 1 !== 0);
+    default:
+      return false;
+  }
+};
+
 class Controller {
   view: View;
 
@@ -67,77 +80,53 @@ class Controller {
 
   private checkStep = (step: string): number => {
     const stepChecked = Math.abs(ParsingDigits.parsing(step));
+    const isStepNumber = stepChecked !== null && stepChecked !== 0;
+    const isStepMoreThanHalf = stepChecked
+      > (this.view.parameters.maxValue - this.view.parameters.minValue) / 2;
 
-    if (this.isStepValueWrong(stepChecked)) {
+    if (!isStepNumber || isStepMoreThanHalf) {
       return this.view.parameters.step;
     }
 
     if (stepChecked % 1 !== 0) {
       this.view.parameters.isFloat = true;
-    } else if (!this.isOthersValuesFloat('step')) {
+    } else if (!isOthersValuesFloat(this, 'step')) {
       this.view.parameters.isFloat = false;
     }
 
     return stepChecked;
   }
 
-  private isStepValueWrong = (stepChecked: number): boolean => {
-    const isStepMoreThanHalf = stepChecked
-      > (this.view.parameters.maxValue - this.view.parameters.minValue) / 2;
-    return (stepChecked === null || stepChecked === 0 || isStepMoreThanHalf);
-  }
-
-  private isOthersValuesFloat = (parameter: string): boolean => {
-    switch (parameter) {
-      case 'step':
-        return (this.view.parameters.maxValue % 1 !== 0 || this.view.parameters.minValue % 1 !== 0);
-      case 'maxValue':
-        return (this.view.parameters.step % 1 !== 0 || this.view.parameters.minValue % 1 !== 0);
-      case 'minValue':
-        return (this.view.parameters.step % 1 !== 0 || this.view.parameters.maxValue % 1 !== 0);
-      default:
-        return false;
-    }
-  }
-
   private checkMaxValue = (maxValue: string): number => {
     const maxValueChecked = ParsingDigits.parsing(maxValue);
 
-    if (this.isMaxValueWrong(maxValueChecked)) {
+    if (maxValueChecked === null || maxValueChecked <= this.view.parameters.minValue) {
       return this.view.parameters.maxValue;
     }
 
     if (maxValueChecked % 1 !== 0) {
       this.view.parameters.isFloat = true;
-    } else if (!this.isOthersValuesFloat('maxValue')) {
+    } else if (!isOthersValuesFloat(this, 'maxValue')) {
       this.view.parameters.isFloat = false;
     }
 
     return maxValueChecked;
   }
 
-  private isMaxValueWrong = (maxValueChecked: number): boolean => {
-    return (maxValueChecked === null || maxValueChecked <= this.view.parameters.minValue);
-  }
-
   private checkMinValue = (minValue: string): number => {
     const minValueChecked = ParsingDigits.parsing(minValue);
 
-    if (this.isMinValueWrong(minValueChecked)) {
+    if (minValueChecked === null || minValueChecked >= this.view.parameters.maxValue) {
       return this.view.parameters.minValue;
     }
 
     if (minValueChecked % 1 !== 0) {
       this.view.parameters.isFloat = true;
-    } else if (!this.isOthersValuesFloat('minValue')) {
+    } else if (!isOthersValuesFloat(this, 'minValue')) {
       this.view.parameters.isFloat = false;
     }
 
     return minValueChecked;
-  }
-
-  private isMinValueWrong = (minValueChecked: number): boolean => {
-    return (minValueChecked === null || minValueChecked >= this.view.parameters.maxValue);
   }
 
   private addViewObserver = (): void => {
