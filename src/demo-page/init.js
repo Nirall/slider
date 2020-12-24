9a579c97f5128f68965650cc64bc1acda774c97a
Aleksey <nirall@mail.ru>
2020-12-24 23:07:56 +0500

    Issue #10. intermeidate refactoring of the demo-page

--- src/demo-page/init.js
+++ src/demo-page/init.js
@@ -1,83 +1,90 @@
 /* global $ */
 
 const handleCurrentMinValueInputFocusout = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('setValues', { currentMinValue: parseFloat(e.data.inputElement.val()) });
+  e.data.entry.omfgslider('setValues', { currentMinValue: parseFloat(e.data.inputElement.val()) });
 };
 
 const handleCurrentMaxValueInputFocusout = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('setValues', { currentMaxValue: parseFloat(e.data.inputElement.val()) });
+  e.data.entry.omfgslider('setValues', { currentMaxValue: parseFloat(e.data.inputElement.val()) });
 };
 
 const handleMinValueInputFocusout = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { minValue: parseFloat($(`${elementID} + .input-wrapper .minValue`).val()) });
+  e.data.entry.omfgslider('update', { minValue: parseFloat(e.data.inputElement.val()) });
 };
 
 const handleMaxValueInputFocusout = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { maxValue: parseFloat($(`${elementID} + .input-wrapper .maxValue`).val()) });
+  e.data.entry.omfgslider('update', { maxValue: parseFloat(e.data.inputElement.val()) });
 };
 
 const handleStepInputFocusout = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { step: parseFloat($(`${elementID} + .input-wrapper .step`).val()) });
+  e.data.entry.omfgslider('update', { step: parseFloat(e.data.inputElement.val()) });
 };
 
 const handleRangeButtonChange = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { isRange: 'toggle' });
+  e.data.entry.omfgslider('update', { isRange: 'toggle' });
 };
 
 const handleVerticalButtonChange = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { isVertical: 'toggle' });
+  e.data.entry.omfgslider('update', { isVertical: 'toggle' });
 };
 
 const handleLabelButtonChange = (e) => {
-  const elementID = e.data.elementName;
-  $(`${elementID}`).omfgslider('update', { showLabel: 'toggle' });
+  e.data.entry.omfgslider('update', { showLabel: 'toggle' });
 };
 
-$('#s1').omfgslider({ showLabel: true });
-$('#s2').omfgslider({
-  minValue: -1000, maxValue: 0, step: 100, isRange: true, showLabel: true
-});
-$('#s3').omfgslider({
-  minValue: 0, maxValue: 1, step: 0.05, isVertical: true, showLabel: true, isFloat: true
-});
-$('#s4').omfgslider({
-  minValue: -3,
-  maxValue: 0,
-  step: 0.1,
-  isRange: true,
-  isVertical: true,
-  showLabel: true,
-  isFloat: true
-});
-['#s1', '#s2', '#s3', '#s4'].forEach((i) => {
-  const currentMinValueInput = $(`${i} + .input-wrapper .minValueIn`);
-  const currentMaxValueInput = $(`${i} + .input-wrapper .maxValueIn`);
-  const minValue = $(`${i} + .input-wrapper .minValue`);
-  const maxValue = $(`${i} + .input-wrapper .maxValue`);
-  const step = $(`${i} + .input-wrapper .step`);
-  $(`${i}`).omfgslider('inputsAttach', {
+const params = [
+  { showLabel: true },
+  {
+    minValue: -1000,
+    maxValue: 0,
+    step: 100,
+    isRange: true,
+    showLabel: true
+  },
+  {
+    minValue: 0,
+    maxValue: 1,
+    step: 0.05,
+    isVertical: true,
+    showLabel: true,
+    isFloat: true
+  },
+  {
+    minValue: -3,
+    maxValue: 0,
+    step: 0.1,
+    isRange: true,
+    isVertical: true,
+    showLabel: true,
+    isFloat: true
+  }
+];
+
+const slidersWrappers = $('.js-slider-wrapper');
+slidersWrappers.each((index, elem) => {
+  const slider = $(elem).find('.js-slider');
+  slider.omfgslider(params[index]);
+  const currentMinValueInput = $(elem).find('.js-minValueIn');
+  const currentMaxValueInput = $(elem).find('.js-maxValueIn');
+  const minValue = $(elem).find('.js-minValue');
+  const maxValue = $(elem).find('.js-maxValue');
+  const step = $(elem).find('.js-step');
+  slider.omfgslider('inputsAttach', {
     minValueInput: currentMinValueInput,
     maxValueInput: currentMaxValueInput,
     maxValue: maxValue,
     minValue: minValue,
     step: step
   });
-  $(`${i}`).omfgslider('renew');
+  slider.omfgslider('renew');
 
-  currentMinValueInput.on('focusout', { elementName: i, inputElement: currentMinValueInput }, handleCurrentMinValueInputFocusout);
-  currentMaxValueInput.on('focusout', { elementName: i, inputElement: currentMaxValueInput }, handleCurrentMaxValueInputFocusout);
-  minValue.on('focusout', { elementName: i }, handleMinValueInputFocusout);
-  maxValue.on('focusout', { elementName: i }, handleMaxValueInputFocusout);
-  step.on('focusout', { elementName: i }, handleStepInputFocusout);
+  currentMinValueInput.on('focusout', { entry: slider, inputElement: currentMinValueInput }, handleCurrentMinValueInputFocusout);
+  currentMaxValueInput.on('focusout', { entry: slider, inputElement: currentMaxValueInput }, handleCurrentMaxValueInputFocusout);
+  minValue.on('focusout', { entry: slider, inputElement: minValue }, handleMinValueInputFocusout);
+  maxValue.on('focusout', { entry: slider, inputElement: maxValue }, handleMaxValueInputFocusout);
+  step.on('focusout', { entry: slider, inputElement: step }, handleStepInputFocusout);
 
-  $(`${i} + .input-wrapper input[name = "range"]`).on('change', { elementName: i }, handleRangeButtonChange);
-  $(`${i} + .input-wrapper input[name = "vertical"]`).on('change', { elementName: i }, handleVerticalButtonChange);
-  $(`${i} + .input-wrapper input[name = "showLabel"]`).on('change', { elementName: i }, handleLabelButtonChange);
+  $(elem).find('.js-range').on('change', { entry: slider }, handleRangeButtonChange);
+  $(elem).find('.js-vertical').on('change', { entry: slider }, handleVerticalButtonChange);
+  $(elem).find('.js-label').on('change', { entry: slider }, handleLabelButtonChange);
 });
