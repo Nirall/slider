0eea8a67b3bfae8de89fcdcab42c8e943705bf14
Aleksey <nirall@mail.ru>
2021-03-13 13:45:00 +0500

    Issue #27. Removing negative margins

--- src/plugin/View/blocks/Scale/Mark/Mark.ts
+++ src/plugin/View/blocks/Scale/Mark/Mark.ts
@@ -33,12 +33,12 @@ class Mark {
     if (this.isVertical) {
       this.elem.style.left = '50%';
       this.elem.style.top = offset + '%';
-      this.elem.style.marginTop = -this.getDimension() / 2 + 'px';
-      this.elem.style.marginLeft = '-45px';
+      this.elem.style.marginTop = '0';
+      this.elem.style.transform = `translate(-45px, ${-this.getDimension() / 2 + 'px'})`;
     } else {
       this.elem.style.top = '50%';
       this.elem.style.left = offset + '%';
-      this.elem.style.marginLeft = -this.getDimension() / 2 + 'px';
+      this.elem.style.transform = `translateX(${-this.getDimension() / 2 + 'px'})`;
       this.elem.style.marginTop = '15px';
     }
   }
@@ -53,12 +53,6 @@ class Mark {
 
   update = (isVertical: boolean): void => {
     this.isVertical = isVertical;
-
-    if (this.isVertical) {
-      this.elem.classList.add('slider__mark_position_vertical');
-    } else {
-      this.elem.classList.remove('slider__mark_position_vertical');
-    }
   }
 
   private init = (observer: types.FunctionCallbackData): void => {
